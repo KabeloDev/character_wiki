3d231702ac50af0d6d7c30cee7a2e4cdc1f1f079
Kabelo Makhanya <makhanyak2002@gmail.com>
2025-07-03 22:20:25 +0200

    Clean architecture > Episode characters

--- character_wiki-main/lib/Character_Wiki/Presentation/Episode/episode.dart
+++ character_wiki-main/lib/Character_Wiki/Presentation/Episode/episode.dart
@@ -1,7 +1,9 @@
 import 'package:character_wiki/Character_Wiki/Presentation/BLoC/Episode/epsiode_bloc.dart';
 import 'package:character_wiki/Character_Wiki/Presentation/BLoC/Episode/epsiode_state.dart';
+import 'package:character_wiki/Character_Wiki/Presentation/Episode/characters_from_episode.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 class EpisodeScreen extends StatelessWidget {
   const EpisodeScreen({super.key});
 
@@ -19,9 +21,12 @@ class EpisodeScreen extends StatelessWidget {
               itemCount: episodes.length,
               itemBuilder: (context, index) {
                 final episode = episodes[index];
-                return ListTile(
+                return ExpansionTile(
                   title: Text(episode.name),
                   subtitle: Text('${episode.episode} • Aired on: ${episode.airDate}'),
+                  children: [
+                    CharactersFromEpisodeScreen(characterUrls: episode.characters),
+                  ],
                 );
               },
             );
